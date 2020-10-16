64c1aa61c2d1fb02b043610daa2ece148cb852f9
lidy0501@163.com <lidy0501@163.com>
2020-10-16 21:01:34 +0800

    基础语法练习

--- pages/demo04/demo04.js
+++ pages/demo04/demo04.js
@@ -1,6 +1,19 @@
 // pages/demo04/demo04.js
 Page({
   data: {
-    
+    num: 0
+  },
+  handleInput(e) {
+    //console.log(e.detail.value)
+    this.setData({
+      num: e.detail.value
+    })
+  },
+  handleTap(e) {
+    //console.log(e.target.dataset.operation)
+    const {operation} = e.currentTarget.dataset
+    this.setData({
+      num: this.data.num + operation
+    })
   }
 })
